95a001c087d4c756d3c41c8d1fd692a9c433e269
WhackaWeasel <weaselcorp@hotmail.com>
2023-09-17 10:49:17 +0900

    Added extra attributes for granular control

--- cog/classes/scheduler_task.py
+++ cog/classes/scheduler_task.py
@@ -5,8 +5,10 @@ from typing import Callable, Coroutine
 
 @dataclass
 class SchedulerTask:
+    id: str
     expires_at: datetime
-    task: Callable[..., None] | Coroutine
+    task: Callable | Coroutine
+    replace: bool = False
 
     def __lt__(self, other):
         return self.expires_at < other.expires_at
