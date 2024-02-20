ba3ab76bac5de448b2a2264b5bdbd1571d38591e
WhackaWeasel <weaselcorp@hotmail.com>
2024-02-20 13:36:07 +1300

    Update lobby_exceptions.py

--- exceptions/lobby_exceptions.py
+++ exceptions/lobby_exceptions.py
@@ -1,6 +1,11 @@
+from discord import Thread, app_commands
+
+
 class LobbyNotFound(Exception):
     """Occurs when server returns a 404 when querying for a lobby."""
+
     pass
+
 
 class MemberNotFound(Exception):
     def __init__(self, member_id: int):
@@ -18,6 +23,12 @@ class GuildNotFound(Exception):
         return self.message
 
 
+class ThreadNotFound(Exception):
+    """Occurs when lobby history_thread_id is None"""
+
+    pass
+
+
 class LobbyChannelNotFound(Exception):
     def __init__(self, lobby_channel_id: int):
         self.message = f"Lobby channel with {lobby_channel_id} not found"
@@ -32,11 +43,24 @@ class LobbyCreationError(Exception):
 
     def __str__(self) -> str:
         return self.message
-    
+
 
 class DeletedLobby(Exception):
     """Occurs when the server has deleted a lobby."""
+
     pass
+
 
 class DeletedGame(Exception):
     pass
+
+
+class MemberAlreadyInLobby(app_commands.AppCommandError):
+    """Occurs when a member joins a lobby while already being in one."""
+
+    def __init__(self, display_name: str, lobby_id: int, thread: Thread):
+        self.message = f"{display_name}, you are already in another lobby with ID: {lobby_id}."
+        self.thread = thread
+
+    def __str__(self) -> str:
+        return self.message
